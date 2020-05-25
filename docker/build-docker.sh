07e26d90315a258b9cc1f492a317ddc9a0215fc2
shikishima-TasakiLab <160442065@ccalumni.meijo-u.ac.jp>
2020-05-25 14:31:41 +0900

    L4Tのバージョンを自動選択するよう修正

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 BUILD_DIR=$(dirname $(readlink -f $0))
 
-docker build -t jetson/ros:melodic-desktop-full ${BUILD_DIR}
+ntr_arr=( $(echo $(cat /etc/nv_tegra_release) | tr -s ',' ' ') )
+MAJOR_VERSION=${ntr_arr[1]}
+MINOR_VERSION=${ntr_arr[4]}
+
+docker build \
+    -t jetson/ros:melodic-desktop-full \
+    --build-arg L4T_VERSION="${MAJOR_VERSION,,}.${MINOR_VERSION}" \
+    ${BUILD_DIR}/src
+
 if [[ $? != 0 ]]; then
     echo "エラーにより中断しました．"
     exit 1
