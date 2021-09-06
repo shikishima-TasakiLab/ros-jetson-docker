082f79668d2aece6256f3566800ac14f176d736c
shikishima <160442065@ccalumni.meijo-u.ac.jp>
2021-09-06 18:03:55 +0900

    r32.5.1に対応

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -5,6 +5,10 @@ ntr_arr=( $(echo $(cat /etc/nv_tegra_release) | tr -s ',' ' ') )
 MAJOR_VERSION=${ntr_arr[1]}
 MINOR_VERSION=${ntr_arr[4]}
 
+if [[ ${MINOR_VERSION} == '5.1' ]]; then
+    MINOR_VERSION='5.0'
+fi
+
 docker build \
     -t "jetson/ros:${MAJOR_VERSION,,}.${MINOR_VERSION}-melodic" \
     --build-arg L4T_VERSION="${MAJOR_VERSION,,}.${MINOR_VERSION}" \
